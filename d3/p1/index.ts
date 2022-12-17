b668f42ed73529b71012f7281b959dd3a7dcf7bb
Pat Smuk <pat.smuk@gmail.com>
2022-12-17 17:13:57 -0500

    Day 3

--- d3/p1/index.ts
+++ d3/p1/index.ts
@@ -1,8 +1,33 @@
 import { readInputFileLines } from '../../util'
 
 function parseLine(line: string) {
-  return line
+  return [
+    new Set(line.slice(0, line.length / 2).split('')),
+    new Set(line.slice(line.length / 2).split(''))
+  ]
+}
+
+function charToPriority(char: string): number {
+  const code = char.charCodeAt(0)
+
+  if (code >= 0x41 && code <= 0x5a) {
+    // Uppercase
+    return code - 0x41 + 27
+  } else {
+    // Lowercase
+    return code - 0x61 + 1
+  }
 }
 
 const inputs = readInputFileLines(__dirname, parseLine)
-console.log(inputs)
+let sum = 0
+
+for (const [left, right] of inputs) {
+  for (const char of left) {
+    if (right.has(char)) {
+      sum += charToPriority(char)
+    }
+  }
+}
+
+console.log(sum)
--- d3/p2/index.ts
+++ d3/p2/index.ts
@@ -1,8 +1,31 @@
 import { readInputFileLines } from '../../util'
+import { batch } from 'iter-tools'
 
 function parseLine(line: string) {
-  return line
+  return new Set(line.split(''))
+}
+
+function charToPriority(char: string): number {
+  const code = char.charCodeAt(0)
+
+  if (code >= 0x41 && code <= 0x5a) {
+    // Uppercase
+    return code - 0x41 + 27
+  } else {
+    // Lowercase
+    return code - 0x61 + 1
+  }
 }
 
 const inputs = readInputFileLines(__dirname, parseLine)
-console.log(inputs)
+let sum = 0
+
+for (const [elf1, elf2, elf3] of batch(3, inputs)) {
+  for (const char of elf1) {
+    if (elf2.has(char) && elf3.has(char)) {
+      sum += charToPriority(char)
+    }
+  }
+}
+
+console.log(sum)
