37860f484402203053933e23751324638f0ca3be
Pat Smuk <pat.smuk@gmail.com>
2022-12-17 16:34:25 -0500

    Day 1

--- d1/p2/index.ts
+++ d1/p2/index.ts
@@ -1,8 +1,21 @@
 import { readInputFileLines } from '../../util'
 
 function parseLine(line: string) {
-  return line
+  return parseInt(line)
 }
 
 const inputs = readInputFileLines(__dirname, parseLine)
-console.log(inputs)
+
+const elfCalories = []
+let totalCalories = 0
+for (const cals of inputs) {
+  if (isNaN(cals)) {
+    elfCalories.push(totalCalories)
+    totalCalories = 0
+  } else {
+    totalCalories += cals
+  }
+}
+
+elfCalories.sort((a, b) => b - a)
+console.log(elfCalories[0] + elfCalories[1] + elfCalories[2])
