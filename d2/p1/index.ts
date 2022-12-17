e5745787194ec1b433e73f3adb96b01fef7dbd37
Pat Smuk <pat.smuk@gmail.com>
2022-12-17 16:50:34 -0500

    Day 2

--- d2/p1/index.ts
+++ d2/p1/index.ts
@@ -1,8 +1,75 @@
 import { readInputFileLines } from '../../util'
 
-function parseLine(line: string) {
-  return line
+type Choice = 'rock' | 'paper' | 'scissors'
+
+const INPUT_TO_CHOICE: {[letter: string]: Choice} = {
+  'A': 'rock',
+  'B': 'paper',
+  'C': 'scissors',
+  'X': 'rock',
+  'Y': 'paper',
+  'Z': 'scissors'
+}
+
+const WIN = 6
+const LOSE = 0
+const DRAW = 3
+const ROCK = 1
+const PAPER = 2
+const SCISSORS = 3
+
+function parseLine(line: string): [Choice, Choice] {
+  return [INPUT_TO_CHOICE[line[0]], INPUT_TO_CHOICE[line[2]]]
 }
 
 const inputs = readInputFileLines(__dirname, parseLine)
-console.log(inputs)
+let totalScore = 0
+
+for (const [opponent, player] of inputs) {
+  switch (opponent) {
+    case 'rock': {
+      switch (player) {
+        case 'rock': {
+          totalScore += DRAW + ROCK; break
+        }
+        case 'paper': {
+          totalScore += WIN + PAPER; break
+        }
+        case 'scissors': {
+          totalScore += LOSE + SCISSORS; break
+        }
+      }
+      break
+    }
+    case 'paper': {
+      switch (player) {
+        case 'rock': {
+          totalScore += LOSE + ROCK; break
+        }
+        case 'paper': {
+          totalScore += DRAW + PAPER; break
+        }
+        case 'scissors': {
+          totalScore += WIN + SCISSORS; break
+        }
+      }
+      break
+    }
+    case 'scissors': {
+      switch (player) {
+        case 'rock': {
+          totalScore += WIN + ROCK; break
+        }
+        case 'paper': {
+          totalScore += LOSE + PAPER; break
+        }
+        case 'scissors': {
+          totalScore += DRAW + SCISSORS; break
+        }
+      }
+      break
+    }
+  }
+}
+
+console.log(totalScore)
--- d2/p2/index.ts
+++ d2/p2/index.ts
@@ -1,8 +1,78 @@
 import { readInputFileLines } from '../../util'
 
-function parseLine(line: string) {
-  return line
+type Choice = 'rock' | 'paper' | 'scissors'
+type Outcome = 'win' | 'lose' | 'draw'
+
+const INPUT_TO_CHOICE: {[letter: string]: Choice} = {
+  'A': 'rock',
+  'B': 'paper',
+  'C': 'scissors'
+}
+const INPUT_TO_OUTCOME: {[letter: string]: Outcome} = {
+  'X': 'lose',
+  'Y': 'draw',
+  'Z': 'win'
+}
+
+const WIN = 6
+const LOSE = 0
+const DRAW = 3
+const ROCK = 1
+const PAPER = 2
+const SCISSORS = 3
+
+function parseLine(line: string): [Choice, Outcome] {
+  return [INPUT_TO_CHOICE[line[0]], INPUT_TO_OUTCOME[line[2]]]
 }
 
 const inputs = readInputFileLines(__dirname, parseLine)
-console.log(inputs)
+let totalScore = 0
+
+for (const [opponentChoice, outcome] of inputs) {
+  switch (opponentChoice) {
+    case 'rock': {
+      switch (outcome) {
+        case 'win': {
+          totalScore += WIN + PAPER; break
+        }
+        case 'lose': {
+          totalScore += LOSE + SCISSORS; break
+        }
+        case 'draw': {
+          totalScore += DRAW + ROCK; break
+        }
+      }
+      break
+    }
+    case 'paper': {
+      switch (outcome) {
+        case 'win': {
+          totalScore += WIN + SCISSORS; break
+        }
+        case 'lose': {
+          totalScore += LOSE + ROCK; break
+        }
+        case 'draw': {
+          totalScore += DRAW + PAPER; break
+        }
+      }
+      break
+    }
+    case 'scissors': {
+      switch (outcome) {
+        case 'win': {
+          totalScore += WIN + ROCK; break
+        }
+        case 'lose': {
+          totalScore += LOSE + PAPER; break
+        }
+        case 'draw': {
+          totalScore += DRAW + SCISSORS; break
+        }
+      }
+      break
+    }
+  }
+}
+
+console.log(totalScore)
