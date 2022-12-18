13fec1519df33d9a689298b862643cffe30b30b3
Pat Smuk <pat.smuk@gmail.com>
2022-12-17 19:48:21 -0500

    Day 4

--- d4/p1/index.ts
+++ d4/p1/index.ts
@@ -1,8 +1,16 @@
 import { readInputFileLines } from '../../util'
 
-function parseLine(line: string) {
-  return line
+function parseLine(line: string): number[][] {
+  return line.split(',').map(part => part.split('-').map(x => parseInt(x)))
 }
 
 const inputs = readInputFileLines(__dirname, parseLine)
-console.log(inputs)
+let numOverlapping = 0
+
+for (const [[start1, end1], [start2, end2]] of inputs) {
+  if ((start1 >= start2 && end1 <= end2) || (start2 >= start1 && end2 <= end1)) {
+    numOverlapping++
+  }
+}
+
+console.log(numOverlapping)
