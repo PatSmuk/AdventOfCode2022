06ea392e24ba160609fc4fce364127e508978bfb
Pat Smuk <pat.smuk@gmail.com>
2022-12-20 01:12:45 -0500

    Day 20

--- d20/p1/index.ts
+++ d20/p1/index.ts
@@ -1,8 +1,29 @@
 import { readInputFileLines } from '../../util'
 
 function parseLine(line: string) {
-  return line
+  return parseInt(line)
 }
 
-const inputs = readInputFileLines(__dirname, parseLine)
-console.log(JSON.stringify(inputs, null, 2))
+let state = readInputFileLines(__dirname, parseLine)
+
+let order = [] as number[]
+for (let i = 0; i < state.length; i++) {
+  order.push(i)
+}
+
+for (let i = 0; i < state.length; i++) {
+  let index = order.findIndex(x => x === i)
+  const num = state[index]
+
+  state.splice(index, 1)
+  order.splice(index, 1)
+  index = (index + num) % state.length
+  state.splice(index, 0, num)
+  order.splice(index, 0, i)
+}
+
+const zeroIndex = state.findIndex(x => x === 0)
+console.log(state[(zeroIndex + 1000) % state.length])
+console.log(state[(zeroIndex + 2000) % state.length])
+console.log(state[(zeroIndex + 3000) % state.length])
+console.log(state[(zeroIndex + 1000) % state.length] + state[(zeroIndex + 2000) % state.length] + state[(zeroIndex + 3000) % state.length])
--- d20/p2/index.ts
+++ d20/p2/index.ts
@@ -1,8 +1,32 @@
 import { readInputFileLines } from '../../util'
 
 function parseLine(line: string) {
-  return line
+  return BigInt(line) * 811589153n
 }
 
-const inputs = readInputFileLines(__dirname, parseLine)
-console.log(JSON.stringify(inputs, null, 2))
+let state = readInputFileLines(__dirname, parseLine)
+const numInputs = BigInt(state.length - 1)
+
+let order = [] as number[]
+for (let i = 0; i < state.length; i++) {
+  order.push(i)
+}
+
+for (let mix = 0; mix < 10; mix++) {
+  for (let i = 0; i < state.length; i++) {
+    let index = order.findIndex(x => x === i)
+    const num = state[index]
+
+    state.splice(index, 1)
+    order.splice(index, 1)
+    index = Number((BigInt(index) + num) % numInputs)
+    state.splice(index, 0, num)
+    order.splice(index, 0, i)
+  }
+}
+
+const zeroIndex = state.findIndex(x => x === 0n)
+console.log(state[(zeroIndex + 1000) % state.length])
+console.log(state[(zeroIndex + 2000) % state.length])
+console.log(state[(zeroIndex + 3000) % state.length])
+console.log(state[(zeroIndex + 1000) % state.length] + state[(zeroIndex + 2000) % state.length] + state[(zeroIndex + 3000) % state.length])
