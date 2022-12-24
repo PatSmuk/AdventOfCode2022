96d0a8206d6214f57ef09cb2f343509a5754c4c4
Pat Smuk <pat.smuk@gmail.com>
2022-12-23 20:37:53 -0500

    Day 8

--- d08/p1/index.ts
+++ d08/p1/index.ts
@@ -1,8 +1,70 @@
 import { readInputFileLines } from '../../util'
 
 function parseLine(line: string) {
-  return line
+  return line.split('').map(x => parseInt(x))
 }
 
-const inputs = readInputFileLines(__dirname, parseLine)
-console.log(JSON.stringify(inputs, null, 2))
+const rows = readInputFileLines(__dirname, parseLine)
+
+// Outer ring of trees is visible.
+let numVisible = rows.length * 2 + (rows[0].length - 2) * 2
+
+for (let y = 1; y < rows.length - 1; y++) {
+  const row = rows[y]
+  for (let x = 1; x < row.length - 1; x++) {
+    const height = row[x]
+
+    // Check visibility from the north.
+    let isVisible = true
+    for (let y2 = y - 1; y2 >= 0; y2--) {
+      if (rows[y2][x] >= height) {
+        isVisible = false
+        break
+      }
+    }
+    if (isVisible) {
+      numVisible++
+      continue
+    }
+
+    // Check visibility from the west.
+    isVisible = true
+    for (let x2 = x - 1; x2 >= 0; x2--) {
+      if (row[x2] >= height) {
+        isVisible = false
+        break
+      }
+    }
+    if (isVisible) {
+      numVisible++
+      continue
+    }
+
+    // Check visibility from the south.
+    isVisible = true
+    for (let y2 = y + 1; y2 < rows.length; y2++) {
+      if (rows[y2][x] >= height) {
+        isVisible = false
+        break
+      }
+    }
+    if (isVisible) {
+      numVisible++
+      continue
+    }
+
+    // Check visibility from the east.
+    isVisible = true
+    for (let x2 = x + 1; x2 < rows[0].length; x2++) {
+      if (row[x2] >= height) {
+        isVisible = false
+        break
+      }
+    }
+    if (isVisible) {
+      numVisible++
+    }
+  }
+}
+
+console.log(numVisible)
--- d08/p2/index.ts
+++ d08/p2/index.ts
@@ -1,8 +1,49 @@
 import { readInputFileLines } from '../../util'
 
 function parseLine(line: string) {
-  return line
+  return line.split('').map(x => parseInt(x))
 }
 
-const inputs = readInputFileLines(__dirname, parseLine)
-console.log(JSON.stringify(inputs, null, 2))
+const rows = readInputFileLines(__dirname, parseLine)
+let highestScenicScore = 0
+
+for (let y = 1; y < rows.length - 1; y++) {
+  const row = rows[y]
+
+  for (let x = 1; x < row.length - 1; x++) {
+    let height = row[x]
+    let scenicScore = 1
+
+    // Check how many trees can be seen to the west.
+    let x2 = x - 1
+    while (row[x2] < height && x2 > 0) {
+      x2--
+    }
+    scenicScore *= x - x2
+
+    // Check how many trees can be seen to the east.
+    x2 = x + 1
+    while (row[x2] < height && x2 < row.length - 1) {
+      x2++
+    }
+    scenicScore *= x2 - x
+
+    // Check how many trees can be seen to the north.
+    let y2 = y - 1
+    while (rows[y2][x] < height && y2 > 0) {
+      y2--
+    }
+    scenicScore *= y - y2
+
+    // Check how many trees can be seen to the south.
+    y2 = y + 1
+    while (rows[y2][x] < height && y2 < rows.length - 1) {
+      y2++
+    }
+    scenicScore *= y2 - y
+
+    highestScenicScore = Math.max(scenicScore, highestScenicScore)
+  }
+}
+
+console.log(highestScenicScore)
