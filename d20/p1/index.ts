56af282360b9a474ae21df0115e502b25c8a50da
Pat Smuk <pat.smuk@gmail.com>
2022-12-23 23:45:56 -0500

    Comment day 20

--- d20/p1/index.ts
+++ d20/p1/index.ts
@@ -6,24 +6,26 @@ function parseLine(line: string) {
 
 let state = readInputFileLines(__dirname, parseLine)
 
+// Track the original indices separately.
 let order = [] as number[]
 for (let i = 0; i < state.length; i++) {
   order.push(i)
 }
 
 for (let i = 0; i < state.length; i++) {
+  // Find index of next number to move.
   let index = order.findIndex(x => x === i)
   const num = state[index]
 
+  // Remove it from both arrays.
   state.splice(index, 1)
   order.splice(index, 1)
+
+  // Compute new index and insert into both arrays.
   index = (index + num) % state.length
   state.splice(index, 0, num)
   order.splice(index, 0, i)
 }
 
 const zeroIndex = state.findIndex(x => x === 0)
-console.log(state[(zeroIndex + 1000) % state.length])
-console.log(state[(zeroIndex + 2000) % state.length])
-console.log(state[(zeroIndex + 3000) % state.length])
 console.log(state[(zeroIndex + 1000) % state.length] + state[(zeroIndex + 2000) % state.length] + state[(zeroIndex + 3000) % state.length])
--- d20/p2/index.ts
+++ d20/p2/index.ts
@@ -7,6 +7,7 @@ function parseLine(line: string) {
 let state = readInputFileLines(__dirname, parseLine)
 const numInputs = BigInt(state.length - 1)
 
+// Track the original indices separately.
 let order = [] as number[]
 for (let i = 0; i < state.length; i++) {
   order.push(i)
@@ -14,11 +15,15 @@ for (let i = 0; i < state.length; i++) {
 
 for (let mix = 0; mix < 10; mix++) {
   for (let i = 0; i < state.length; i++) {
+    // Find index of next number to move.
     let index = order.findIndex(x => x === i)
     const num = state[index]
 
+    // Remove it from both arrays.
     state.splice(index, 1)
     order.splice(index, 1)
+
+    // Compute new index and insert into both arrays.
     index = Number((BigInt(index) + num) % numInputs)
     state.splice(index, 0, num)
     order.splice(index, 0, i)
@@ -26,7 +31,4 @@ for (let mix = 0; mix < 10; mix++) {
 }
 
 const zeroIndex = state.findIndex(x => x === 0n)
-console.log(state[(zeroIndex + 1000) % state.length])
-console.log(state[(zeroIndex + 2000) % state.length])
-console.log(state[(zeroIndex + 3000) % state.length])
 console.log(state[(zeroIndex + 1000) % state.length] + state[(zeroIndex + 2000) % state.length] + state[(zeroIndex + 3000) % state.length])
