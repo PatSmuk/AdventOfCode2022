6e9f6153ec35574547f18cf7b2fb620bd0be5819
Pat Smuk <pat.smuk@gmail.com>
2022-12-24 18:33:11 -0500

    Day 10

--- d10/p1/index.ts
+++ d10/p1/index.ts
@@ -1,8 +1,24 @@
 import { readInputFileLines } from '../../util'
 
 function parseLine(line: string) {
-  return line
+  const parts = line.split(' ')
+
+  // Handle "nop"
+  if (parts.length === 1) {
+    return [0]
+  }
+
+  // Handle "addx <amount>"
+  return [0, parseInt(parts[1])]
 }
 
-const inputs = readInputFileLines(__dirname, parseLine)
-console.log(JSON.stringify(inputs, null, 2))
+// Flatten into single array of deltas.
+const inputs = readInputFileLines(__dirname, parseLine).flat()
+
+let signalStrengthSum = 0
+for (let cycle = 20; cycle <= 220; cycle += 40) {
+  const signalStrength = inputs.slice(0, cycle - 1).reduce((x, dx) => x + dx, 1)
+  signalStrengthSum += cycle * signalStrength
+}
+
+console.log(signalStrengthSum)
--- d10/p2/index.ts
+++ d10/p2/index.ts
@@ -1,8 +1,32 @@
 import { readInputFileLines } from '../../util'
 
 function parseLine(line: string) {
-  return line
+  const parts = line.split(' ')
+  if (parts.length === 1) {
+    return [0]
+  }
+  return [0, parseInt(parts[1])]
 }
 
-const inputs = readInputFileLines(__dirname, parseLine)
-console.log(JSON.stringify(inputs, null, 2))
+const LINE_LENGTH = 40
+
+// Flatten into single array of deltas.
+const inputs = readInputFileLines(__dirname, parseLine).flat()
+
+let line = ''
+for (let cycle = 0; cycle < inputs.length; cycle++) {
+  const x = cycle % LINE_LENGTH
+  const xRegister = inputs.slice(0, cycle).reduce((prev, curr) => prev + curr, 1)
+
+  if (Math.abs(xRegister - x) <= 1) {
+    line += '#'
+  } else {
+    line += '.'
+  }
+
+  // If we're at the end of the line, print it and reset.
+  if (x === LINE_LENGTH - 1) {
+    console.log(line)
+    line = ''
+  }
+}
