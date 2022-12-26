ac68c4f9bd953e1fbf6d3d35add6eb5d35124807
Pat Smuk <pat.smuk@gmail.com>
2022-12-26 15:22:30 -0500

    Day 11

--- d11/p1/index.ts
+++ d11/p1/index.ts
@@ -1,8 +1,66 @@
-import { readInputFileLines } from '../../util'
-
-function parseLine(line: string) {
-  return line
+export interface Monkey {
+  items: number[]
+  operation: (old: number) => number
+  testDivisor: number
+  divisbleTarget: number
+  notDivisibleTarget: number
+  inspections: number
 }
 
-const inputs = readInputFileLines(__dirname, parseLine)
-console.log(JSON.stringify(inputs, null, 2))
+const monkeys: Monkey[] = [
+  {
+    items: [79, 98],
+    operation: (old: number) => old * 19,
+    testDivisor: 23,
+    divisbleTarget: 2,
+    notDivisibleTarget: 3,
+    inspections: 0
+  },
+  {
+    items: [54, 65, 75, 74],
+    operation: (old: number) => old + 6,
+    testDivisor: 19,
+    divisbleTarget: 2,
+    notDivisibleTarget: 0,
+    inspections: 0
+  },
+  {
+    items: [79, 60, 97],
+    operation: (old: number) => old * old,
+    testDivisor: 13,
+    divisbleTarget: 1,
+    notDivisibleTarget: 3,
+    inspections: 0
+  },
+  {
+    items: [74],
+    operation: (old: number) => old + 3,
+    testDivisor: 17,
+    divisbleTarget: 0,
+    notDivisibleTarget: 1,
+    inspections: 0
+  }
+]
+
+const NUM_ROUNDS = 20
+
+for (let round = 1; round <= NUM_ROUNDS; round++) {
+  for (const monkey of monkeys) {
+    while (monkey.items.length > 0) {
+      monkey.inspections++
+
+      let item = monkey.items.shift()!
+      item = monkey.operation(item)
+      item = Math.floor(item / 3)
+
+      if (item % monkey.testDivisor === 0) {
+        monkeys[monkey.divisbleTarget].items.push(item)
+      } else {
+        monkeys[monkey.notDivisibleTarget].items.push(item)
+      }
+    }
+  }
+}
+
+const [first, second] = monkeys.map(m => m.inspections).sort((a, b) => b - a).slice(0, 2)
+console.log(first * second)
--- d11/p2/index.ts
+++ d11/p2/index.ts
@@ -1,8 +1,88 @@
-import { readInputFileLines } from '../../util'
+import assert from 'assert'
 
-function parseLine(line: string) {
-  return line
+type Item = Map<number, number>
+type Operation = (old: number) => number
+
+const DIVISORS = [23, 19, 13, 17]
+
+function I(n: number): Item {
+  const item = new Map<number, number>()
+
+  for (const divisor of DIVISORS) {
+    item.set(divisor, n % divisor)
+  }
+
+  return item
 }
 
-const inputs = readInputFileLines(__dirname, parseLine)
-console.log(JSON.stringify(inputs, null, 2))
+function applyOperation(item: Item, operation: Operation) {
+  for (const [divisor, old] of item.entries()) {
+    item.set(divisor, operation(old) % divisor)
+  }
+}
+
+interface Monkey {
+  items: Item[]
+  operation: (old: number) => number
+  divisbleTarget: number
+  notDivisibleTarget: number
+  inspections: number
+}
+
+const monkeys: Monkey[] = [
+  {
+    items: [I(79), I(98)],
+    operation: (old: number) => old * 19,
+    divisbleTarget: 2,
+    notDivisibleTarget: 3,
+    inspections: 0
+  },
+  {
+    items: [I(54), I(65), I(75), I(74)],
+    operation: (old: number) => old + 6,
+    divisbleTarget: 2,
+    notDivisibleTarget: 0,
+    inspections: 0
+  },
+  {
+    items: [I(79), I(60), I(97)],
+    operation: (old: number) => old * old,
+    divisbleTarget: 1,
+    notDivisibleTarget: 3,
+    inspections: 0
+  },
+  {
+    items: [I(74)],
+    operation: (old: number) => old + 3,
+    divisbleTarget: 0,
+    notDivisibleTarget: 1,
+    inspections: 0
+  }
+]
+
+assert(monkeys.length === DIVISORS.length)
+
+const NUM_ROUNDS = 10000
+
+for (let round = 1; round <= NUM_ROUNDS; round++) {
+  for (let i = 0; i < monkeys.length; i++) {
+    const monkey = monkeys[i]
+    const divisor = DIVISORS[i]
+
+    while (monkey.items.length > 0) {
+      monkey.inspections++
+
+      const item = monkey.items.shift()!
+      applyOperation(item, monkey.operation)
+
+      if (item.get(divisor) === 0) {
+        monkeys[monkey.divisbleTarget].items.push(item)
+      } else {
+        monkeys[monkey.notDivisibleTarget].items.push(item)
+      }
+    }
+  }
+}
+
+const [first, second] = monkeys.map(m => m.inspections).sort((a, b) => b - a).slice(0, 2)
+console.log(first * second)
