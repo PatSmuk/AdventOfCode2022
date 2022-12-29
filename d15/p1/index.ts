9afa409a1b0b160ae93adc3cd70e6573a87181f2
Pat Smuk <pat.smuk@gmail.com>
2022-12-29 16:53:17 -0500

    Day 15

--- d15/p1/index.ts
+++ d15/p1/index.ts
@@ -1,8 +1,48 @@
+import assert from 'assert'
 import { readInputFileLines } from '../../util'
 
+// To run example, swap the comments here:
+const TARGET_Y = 2000000
+// const TARGET_Y = 10
+
+const INPUT_PATTERN = /Sensor at x=(-?\d+), y=(-?\d+): closest beacon is at x=(-?\d+), y=(-?\d+)/
+
 function parseLine(line: string) {
-  return line
+  const match = INPUT_PATTERN.exec(line)
+  assert(match)
+  const [sensorX, sensorY, beaconX, beaconY] = match.slice(1).map(x => parseInt(x))
+  return { sensorX, sensorY, beaconX, beaconY }
 }
 
+// Track how many positions at TARGET_Y have no beacon.
+const noBeaconPositions = new Set<number>()
+
 const inputs = readInputFileLines(__dirname, parseLine)
-console.log(JSON.stringify(inputs, null, 2))
+
+const beaconPositions = new Set<string>()
+const sensorPositions = new Set<string>()
+for (const { sensorX, sensorY, beaconX, beaconY } of inputs) {
+  beaconPositions.add(`${beaconX},${beaconY}`)
+  sensorPositions.add(`${sensorX},${sensorY}`)
+}
+
+for (const { sensorX, sensorY, beaconX, beaconY } of inputs) {
+  // Figure out the distance sensor to target Y.
+  const sensorRange = Math.abs(beaconX - sensorX) + Math.abs(beaconY - sensorY)
+  const distanceToTargetY = Math.abs(sensorY - TARGET_Y)
+  const xRangeAtTargetY = sensorRange - distanceToTargetY
+
+  // If target Y is beyond the range of the sensor, skip.
+  if (xRangeAtTargetY < 0) {
+    continue
+  }
+
+  for (let x = sensorX - xRangeAtTargetY; x <= sensorX + xRangeAtTargetY; x++) {
+    // If there's no beacon at this position, at it to set of no beacon positions.
+    if (!beaconPositions.has(`${x},${TARGET_Y}`)) {
+      noBeaconPositions.add(x)
+    }
+  }
+}
+
+console.log(noBeaconPositions.size)
--- d15/p2/index.ts
+++ d15/p2/index.ts
@@ -1,8 +1,48 @@
+import assert from 'assert'
 import { readInputFileLines } from '../../util'
 
+// To run example, swap the comments here:
+const MAX_COORD = 4000000
+// const MAX_COORD = 20
+
+const INPUT_PATTERN = /Sensor at x=(-?\d+), y=(-?\d+): closest beacon is at x=(-?\d+), y=(-?\d+)/
+
 function parseLine(line: string) {
-  return line
+  const match = INPUT_PATTERN.exec(line)
+  assert(match)
+  const [sensorX, sensorY, beaconX, beaconY] = match.slice(1).map(x => parseInt(x))
+  return { sensorX, sensorY, beaconX, beaconY }
 }
 
 const inputs = readInputFileLines(__dirname, parseLine)
-console.log(JSON.stringify(inputs, null, 2))
+
+// Remember every sensor's position and how far it can see.
+const sensorRanges = [] as [number, number, number][]
+for (const { sensorX, sensorY, beaconX, beaconY } of inputs) {
+  const range = Math.abs(beaconX - sensorX) + Math.abs(beaconY - sensorY)
+  sensorRanges.push([sensorX, sensorY, range])
+}
+
+// Check every (x,y) coordinate in range.
+done: for (let x = 0; x < MAX_COORD; x++) {
+  for (let y = 0; y < MAX_COORD; y++) {
+    let isInSensorRange = false
+
+    // Try to find a sensor in range of this coordinate.
+    for (const [sensorX, sensorY, range] of sensorRanges) {
+      const distToSensor = Math.abs(sensorX - x) + Math.abs(sensorY - y)
+      if (distToSensor <= range) {
+        isInSensorRange = true
+        // Optimization: we can skip forward until we are out of range of this sensor on the next iteration.
+        y += range - distToSensor
+        break
+      }
+    }
+
+    // No sensors in range, we found the gap.
+    if (!isInSensorRange) {
+      console.log(x, y, x * 4000000 + y)
+      break done
+    }
+  }
+}
